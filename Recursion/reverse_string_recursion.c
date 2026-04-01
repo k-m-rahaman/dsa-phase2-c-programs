aa35ed92456133bdfc120f044fcd8d201fcfce97
k-m-rahaman <kmrofficial786@gmail.com>
2026-04-01 06:42:52 +0530

    Added recursion programs in C including string reverse

---
+++ Recursion/reverse_string_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char str[], int start, int end) {
+    if (start >= end)
+        return;
+
+    char temp = str[start];
+    str[start] = str[end];
+    str[end] = temp;
+
+    reverse(str, start + 1, end - 1);
+}
+
+int main() {
+    char str[] = "hello";
+
+    reverse(str, 0, strlen(str) - 1);
+    printf("Reversed: %s", str);
+
+    return 0;
+}
