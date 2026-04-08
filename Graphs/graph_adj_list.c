230c5832f2c53b618813a8df87bc4e37456937a5
k-m-rahaman <kmrofficial786@gmail.com>
2026-04-08 05:57:12 +0530

    Added graph representation using adjacency list in C

---
+++ Graphs/graph_adj_list.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void addEdge(struct Node* adj[], int src, int dest) {
+    struct Node* newNode = createNode(dest);
+    newNode->next = adj[src];
+    adj[src] = newNode;
+
+    newNode = createNode(src);
+    newNode->next = adj[dest];
+    adj[dest] = newNode;
+}
+
+void printGraph(struct Node* adj[], int V) {
+    for (int i = 0; i < V; i++) {
+        struct Node* temp = adj[i];
+        printf("%d: ", i);
+        while (temp) {
+            printf("%d -> ", temp->vertex);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+int main() {
+    int V = 5;
+    struct Node* adj[V];
+
+    for (int i = 0; i < V; i++)
+        adj[i] = NULL;
+
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 2);
+    addEdge(adj, 1, 3);
+    addEdge(adj, 2, 4);
+
+    printGraph(adj, V);
+    return 0;
+}
